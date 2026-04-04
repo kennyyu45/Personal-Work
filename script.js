9df68d49e413e930e447322d7e3dc91de68ccd3d
Kenny_Yu <kennylgm45@gmail.com>
2026-04-04 11:50:44 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+(function(){
+    "use strict";
+    console.log("testing js");
+
+    // const introName = document.querySelector("#intro-name");
+
+    // introName.addEventListener("mouseover", function(){
+    //     introName.classList.add("expand");
+    // });
+    // introName.addEventListener("mouseout", function(){
+    //     introName.classList.remove("expand");
+    // });
+})();
